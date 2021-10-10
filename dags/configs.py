1a9810712452a384700e2b60573afb5061daff48
ashen <jayasinghe.ashen@gmail.com>
2021-10-10 19:25:26 +0530

    aws credential bug fixes

--- dags/configs.py
+++
@@ -1,7 +0,0 @@
-# Configurations
-BUCKET_NAME = "my-bucket-ashen"
-LOCAL_DATA = "./dags/data/movie_review.csv"
-S3_DATA = "data/movie_review.csv"
-LOCAL_SCRIPT = "./dags/scripts/spark/random_text_classification.py"
-S3_SCRIPT = "scripts/random_text_classification.py"
-s3_clean = "clean_data/"
--- dags/dummy_operator.py
+++
@@ -1,8 +0,0 @@
-from airflow.operators.dummy_operator import DummyOperator
-
-
-def start_data_pipeline(dag):
-    return DummyOperator(task_id="start_data_pipeline", dag=dag)
-
-def end_data_pipeline(dag):
-    return DummyOperator(task_id="end_data_pipeline", dag=dag)
